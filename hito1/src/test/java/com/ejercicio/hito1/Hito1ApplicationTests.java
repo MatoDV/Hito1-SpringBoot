fd20a732b1510435ea39b952d91fd25d4c9dcbc8
MatoDV <matias.brizuela@davinci.edu.ar>
2024-06-25 19:05:58 -0300

    feat: implementation of folder with the spring boot initializr

---
+++ hito1/src/test/java/com/ejercicio/hito1/Hito1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.hito1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hito1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
